b1416390ab99cd7b1b636bda9d18d745512c3062
OlgaBieliaieva <112978931+OlgaBieliaieva@users.noreply.github.com>
2023-01-20 13:59:05 +0200

    Finished task#2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulIngredientsListRef = document.querySelector('#ingredients');
+
+const elements = ingredients.map(ingredient => {
+  const listItemEl = document.createElement('li');
+  listItemEl.textContent = ingredient;
+  listItemEl.classList.add('item');
+  return listItemEl
+})
+
+ulIngredientsListRef.append(...elements);
