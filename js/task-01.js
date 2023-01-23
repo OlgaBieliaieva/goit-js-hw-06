cf40eed819d830e46310c0d34ff739425ce6636e
OlgaBieliaieva <112978931+OlgaBieliaieva@users.noreply.github.com>
2023-01-23 09:04:15 +0200

    refactored task#1

--- js/task-01.js
+++ js/task-01.js
@@ -8,12 +8,12 @@ console.log(`Number of categories: ${categoriesListRef.childElementCount}`);
 
 // ?  Count the number of elements in each category
 
-const categoriesTitlesRef = document.querySelectorAll('.item h2')
-const categoriesItemsRef = document.querySelectorAll('.item ul')
 
-console.log(`Category: ${categoriesTitlesRef[0].textContent}`);
-console.log(`Elements: ${categoriesItemsRef[0].childElementCount}`);
-console.log(`Category: ${categoriesTitlesRef[1].textContent}`);
-console.log(`Elements: ${categoriesItemsRef[1].childElementCount}`);
-console.log(`Category: ${categoriesTitlesRef[2].textContent}`);
-console.log(`Elements: ${categoriesItemsRef[2].childElementCount}`);
+const categoriesItemsRef = document.querySelectorAll('.item')
+
+const showCategories = categoriesItemsRef.forEach((item) => {
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    console.log(`Elements: ${item.lastElementChild.childElementCount}`);
+})
+
+
