2880efcb3213b83ffd501b8029ce4aa673a512f0
OlgaBieliaieva <112978931+OlgaBieliaieva@users.noreply.github.com>
2023-01-24 14:16:55 +0200

    finished task#5

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const refs = {
+    inputField: document.querySelector("#name-input"),
+    outputField: document.querySelector("#name-output")
+}
+
+refs.inputField.addEventListener('input', onInputChange)
+
+function onInputChange(event) {
+    event.currentTarget.value === "" ? refs.outputField.textContent = "Anonymous" : refs.outputField.textContent = event.currentTarget.value
+}
