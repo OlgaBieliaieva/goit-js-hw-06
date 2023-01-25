b53c01b1edd51c2a78583f520a4bf4e90ba23c5c
OlgaBieliaieva <112978931+OlgaBieliaieva@users.noreply.github.com>
2023-01-25 15:03:20 +0200

    finished task #8

--- js/task-08.js
+++ js/task-08.js
@@ -1,4 +1,22 @@
 const refs = {
     form: document.querySelector('.login-form'),
-    submitBTN: document.querySelector('button')
+}
+
+refs.form.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const formDataObj = {};
+    
+    formData.forEach((value, name) => {
+        
+        if (value.length === 0) {
+            value = prompt(`Fill in the field "${name}", please`);
+        }
+        
+        formDataObj[name] = value
+    })
+    console.log(formDataObj);
+    refs.form.reset();
 }
