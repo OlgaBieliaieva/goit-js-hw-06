9ed917030965f3806168e8a4f5fd955ea80489a9
OlgaBieliaieva <112978931+OlgaBieliaieva@users.noreply.github.com>
2023-02-01 08:46:57 +0200

    debugged task#6

--- js/task-06.js
+++ js/task-06.js
@@ -4,5 +4,6 @@ const inputRef = document.querySelector('#validation-input')
 inputRef.addEventListener('blur', onInputBlur)
 
 function onInputBlur(event) {
-    event.currentTarget.value.length === Number(inputRef.dataset.length) ? inputRef.classList.add("valid") : inputRef.classList.add("invalid")
+    inputRef.classList.remove("valid", "invalid");
+    event.currentTarget.value.length === Number(inputRef.dataset.length) ? inputRef.classList.add("valid") : inputRef.classList.add("invalid");     
 }
